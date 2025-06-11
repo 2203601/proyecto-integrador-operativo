a86be6da7cfe2ace7868b8283f99a98a1ac2be24
Kevinmass <2203817@ucc.edu.ar>
2025-06-11 10:47:27 -0300

    Agregue los puntos 5 y 6 totalmente funcionales y con las verificaciones basicas

--- proyecto.sh
+++ proyecto.sh
@@ -9,6 +9,8 @@
 # e) Manejo de errores
 # f) Cambiar pass
 
+numero=0
+
 while [ $numero -ne 7 ]; do
 
  echo "-- GESTOR DE USUARIOS Y PERMISOS --"
@@ -38,14 +40,40 @@ while [ $numero -ne 7 ]; do
             ;;
 
         4)  echo "Elegiste la opción 4: Modificar informacion de usuario"
-            ;;
-        5)  echo "Elegiste la opción 5: Mostrar informacion de usuario"
-            
+
             ;;
 
-        6)  echo "Elegiste la opción 6: Cambiar password"
-           
+        5)  echo "Elegiste la opción 5: Mostrar informacion de usuario"
+            read -p "Ingrese el nombre del usuario: " usuario
+
+            if id "$usuario" >/dev/null 2>&1; then
+                echo "Información del usuario '$usuario':"
+                getent passwd "$usuario"
+                id "$usuario"
+            else
+                echo "Error: El usuario '$usuario' no existe."
+            fi
+
             ;;
+
+
+        6)  echo "Elegiste la opción 6: Cambiar password"
+            read -p "Ingrese el nombre del usuario: " usuario
+
+            if id "$usuario" >/dev/null 2>&1; then
+                echo "Cambiando contraseña para '$usuario'."
+                sudo passwd "$usuario"
+                if [ $? -ne 0 ]; then
+                    echo "Error: No se pudo cambiar la contraseña de '$usuario'."
+                else
+                    echo "Contraseña cambiada correctamente."
+                fi
+            else
+                echo "Error: El usuario '$usuario' no existe."
+            fi
+
+            ;;
+
 
         7) echo "Saliendo"
             exit 0
